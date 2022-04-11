4ce8985d22f1be38fdd6c73a157051c14827bbd6
Windows <Windows@DESKTOP-TN1A36V>
2022-04-10 22:46:35 -0300

    Atributos iniciales y constructores para clase Autor

--- ProjectDemo/src/main/java/Autor.java
+++ ProjectDemo/src/main/java/Autor.java
@@ -1,4 +1,20 @@
 
 public class Autor {
+	private String nombre;
+	private String libro;
+	private int edad;
+	
+	public Autor() {
+		
+	}
 
+	public Autor(String nombre, String libro, int edad) {
+		super();
+		this.nombre = nombre;
+		this.libro = libro;
+		this.edad = edad;
+	}
+	
+	
+	
 }
